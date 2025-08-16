d97979aac9910cb151579bee5f6988d311c76c30
omar <OmarYoussry2001>
2025-08-16 03:43:28 +0300

    1- Adding Home Module 2- Adding Checkout Module

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home-module').then(m => m.HomeModule) },
   { path: 'shop', loadChildren: () => import('./shop/shop-module').then(m => m.ShopModule) },
   { path: 'basket', loadChildren: () => import('./basket/basket-module').then(m => m.BasketModule) },
-  { path: '', redirectTo: 'shop', pathMatch: 'full' }
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout-module').then(m => m.CheckoutModule) },
+  { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
